59fed12b6817305cfa1aef7d98d84f0f8e7b3dde
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-01 00:53:56 +0300

    stuck

--- Dicee Challenge - Starting Files/script.js
+++ Dicee Challenge - Starting Files/script.js
@@ -1,2 +1,8 @@
-randomNumber1 = Math.floor(Math.random() * 6) + 1; 
-console.log(randomNumber1);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png"; 
+
+// assuming the image folder is in the same directory as your script.js file
+var randomImageSource = "images/" + randomDiceImage; 
+
+// change the source of the img with id="leftDice" to the random image source
+document.getElementById('leftDice').src = randomImageSource;
