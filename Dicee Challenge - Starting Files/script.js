316c90e3903e69132c5376479f06c261148abfd5
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-01 01:11:00 +0300

    COMPLETE

--- Dicee Challenge - Starting Files/script.js
+++ Dicee Challenge - Starting Files/script.js
@@ -1,8 +1,27 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage = "dice" + randomNumber1 + ".png"; 
+var randomDiceImage = "image/disc"+ randomNumber1 + ".png"; 
 
 // assuming the image folder is in the same directory as your script.js file
 var randomImageSource = "images/" + randomDiceImage; 
 
-// change the source of the img with id="leftDice" to the random image source
-document.getElementById('leftDice').src = randomImageSource;
+var image1 = document.querySelectorAll("img")[0];
+
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/disc" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
+
